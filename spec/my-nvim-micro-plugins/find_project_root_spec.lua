f0505fb59d6fe263494362839d3eb9071ae78789
Mika Vilpas <mika.vilpas@gmail.com>
2024-10-12 20:57:41 +0300

    refactor: make find_project_root more testable

--- spec/my-nvim-micro-plugins/find_project_root_spec.lua
+++ spec/my-nvim-micro-plugins/find_project_root_spec.lua
@@ -1,8 +1,43 @@
 local main = require("my-nvim-micro-plugins.main")
+local spy = require("luassert.spy")
+local assert = require("luassert")
 
-it("can find the root when in a .git directory", function()
-  local starting_dir = vim.fn.getcwd()
-  vim.cmd("edit .git/file")
-  local root = main.find_project_root()
-  assert.same(starting_dir, root)
+describe("climbing up from .git", function()
+  it(
+    "retries with the parent directory if the current directory is .git",
+    function()
+      local base_dir = vim.fn.tempname()
+      vim.fn.mkdir(base_dir, "p")
+
+      local git_root_finder_function = spy.new(function()
+        return ".git"
+      end) --[[@as fun(): string]]
+
+      main.find_project_root(base_dir, git_root_finder_function)
+
+      assert.spy(git_root_finder_function).was_called_with(base_dir)
+      assert
+        .spy(git_root_finder_function)
+        .was_called_with(vim.fs.joinpath(base_dir, ".."))
+    end
+  )
+
+  it(
+    "does not retry with the parent directory if the current directory is not exactly '.git'",
+    function()
+      local base_dir = vim.fn.tempname()
+      vim.fn.mkdir(base_dir, "p")
+
+      local git_root_finder_function = spy.new(function()
+        return ".gitsomething"
+      end) --[[@as fun(): string]]
+
+      main.find_project_root(base_dir, git_root_finder_function)
+
+      assert.spy(git_root_finder_function).was_called_with(base_dir)
+      assert
+        .spy(git_root_finder_function)
+        .was_not_called_with(vim.fs.joinpath(base_dir, ".."))
+    end
+  )
 end)
