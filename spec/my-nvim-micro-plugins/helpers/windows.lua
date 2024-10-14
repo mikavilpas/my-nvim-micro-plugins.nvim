1ec2f044ad01d031f349685f5acdf371d26d6705
Mika Vilpas <mika.vilpas@gmail.com>
2024-10-14 20:04:46 +0300

    feat: allow rotating windows like `c-w c-x` exchanges windows

---
+++ spec/my-nvim-micro-plugins/helpers/windows.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.list_windows_and_files()
+  local win_count = vim.fn.winnr("$")
+  local result = {}
+
+  for i = 1, win_count do
+    local bufnr = vim.fn.winbufnr(i)
+    local filename = vim.fn.bufname(bufnr)
+    table.insert(
+      result,
+      string.format(filename == "" and "[No File]" or filename)
+    )
+  end
+
+  return result
+end
+
+function M.get_current_window_file()
+  return vim.fn.expand("%:t")
+end
+
+return M
