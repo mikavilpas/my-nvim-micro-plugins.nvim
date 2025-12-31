71a03698c89fa1078cd736ab68c715a69723f17a
Mika Vilpas <mika.vilpas@gmail.com>
2025-12-31 20:21:31 +0200

    ci: remove committing anything when releasing to simplify (#474)

--- packages/integration-tests/cypress/e2e/healthcheck.cy.ts
+++ packages/integration-tests/cypress/e2e/healthcheck.cy.ts
@@ -7,18 +7,6 @@ describe("the healthcheck", () => {
 
       nvim.runExCommand({ command: "checkhealth my-nvim-micro-plugins" })
 
-      // the version of the plugin should be shown
-      cy.readFile("../../lua/my-nvim-micro-plugins.lua").then(
-        (luaFile: string) => {
-          const match = luaFile.match(/M\.version\s*=\s*"([^"]+)"/)
-          if (!match) {
-            throw new Error("Could not find version in lua file")
-          }
-          const version = match[1]
-          cy.contains(`Running version ${version}`)
-        },
-      )
-
       cy.contains("found 'fd' version")
       cy.contains("found 'rg' version")
       cy.contains("found realpath")
