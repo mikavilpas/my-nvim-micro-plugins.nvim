e0bbcf4c6c28a884368585d81ee3803f15ab0f29
Mika Vilpas <mika.vilpas@gmail.com>
2026-07-23 10:19:14 +0300

    refactor: replace eslint with oxlint and `@mikavilpas/oxlint-config`

---
+++ packages/integration-tests/oxlint.config.ts
@@ -0,0 +1,52 @@
+import mikaConfig from "@mikavilpas/oxlint-config"
+import { defineConfig } from "oxlint"
+
+// oxlint-disable-next-line import/no-default-export
+export default defineConfig({
+  extends: [mikaConfig],
+  jsPlugins: [
+    // https://github.com/levibuzolic/eslint-plugin-no-only-tests#oxlint
+    "eslint-plugin-no-only-tests",
+  ],
+  env: {
+    builtin: true,
+    es2026: true,
+  },
+  ignorePatterns: [
+    "**/vite.config.js",
+    "**/cypress.config.ts",
+    "**/test-environment/",
+    "dist/",
+    "cypress/support/tui-sandbox.ts",
+  ],
+  rules: {
+    "no-only-tests/no-only-tests": "error",
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        fix: {
+          imports: "safe-fix",
+        },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["cypress/e2e/utils/*.ts", "cypress/e2e/*.cy.ts"],
+      rules: {
+        "no-unused-expressions": "off",
+        // cypress does not support the node: protocol
+        "unicorn/prefer-node-protocol": "off",
+        "import/unambiguous": "off",
+      },
+    },
+    {
+      files: ["cypress/e2e/blink-ripgrep/*.cy.ts"],
+      // cypress knows how to import its own files
+      rules: { "import/unambiguous": "allow" },
+    },
+  ],
+})
